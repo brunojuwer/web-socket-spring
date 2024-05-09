ae000d85a081ba91d1f8028e39f56e25fa29a978
Bruno Juwer <bruno.juwer1997@gmail.com>
2024-05-09 20:16:27 -0300

    Created controller

---
+++ src/main/java/com/socket/app/chat/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.socket.app.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType messageType;
+}
+
+
---
+++ src/main/java/com/socket/app/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.socket.app.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVER
+}
